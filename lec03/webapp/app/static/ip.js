ff0b033e18b705da36ee670f49ec83ca0d6942c5
Kotaro <koe.bluebear@gmail.com>
2022-01-24 11:43:08 +0800

    Update hands-on #6

---
+++ lec03/webapp/app/static/ip.js
@@ -0,0 +1,112 @@
+const data = {
+    "students":[
+        {
+            "id":0,
+            "name":"GENG MINGHONG",
+            "short_name":"Minghong",
+            "program":"phd"
+        },
+        {
+            "id":1,
+            "name":"GUO YANRU",
+            "short_name":"Yanru",
+            "program":"non-phd"
+        },
+        {
+            "id":2,
+            "name":"NG HAN WEI",
+            "short_name":"Hanwei",
+            "program":"non-phd"
+        },
+        {
+            "id":3,
+            "name":"JIN XUAN",
+            "short_name":"Jinxuan",
+            "program":"non-phd"
+        },
+        {
+            "id":4,
+            "name":"KANATTA GAMAGE RAMESH DARSHANA RATHNAYAKE",
+            "short_name":"Darshana",
+            "program":"phd"
+        },
+        {
+            "id":5,
+            "name":"LIAW SHAO YI",
+            "short_name":"Shaoyi",
+            "program":"non-phd"
+        },
+        {
+            "id":6,
+            "name":"LOH YI LIN",
+            "short_name":"Yilin",
+            "program":"non-phd"
+        },
+        {
+            "id":7,
+            "name":"LUO ZILIN",
+            "short_name":"Zilin",
+            "program":"phd"
+        },
+        {
+            "id":8,
+            "name":"PITIGALA ARACHCHILLAGE PANSILU MADHURA BHASHANA PITIGALAARACHCHI",
+            "short_name":"Pansilu",
+            "program":"phd"
+        },
+        {
+            "id":9,
+            "name":"PIUS LEE WEI QI",
+            "short_name":"Pius",
+            "program":"non-phd"
+        },
+        {
+            "id":10,
+            "name":"ROMAN LOK-MING BELAIRE",
+            "short_name":"Roman",
+            "program":"phd"
+        },
+        {
+            "id":11,
+            "name":"RUAN SHAOLUN",
+            "short_name":"Shaolun",
+            "program":"phd"
+        },
+        {
+            "id":12,
+            "name":"SIMON TEO BOON KWANG",
+            "short_name":"Simon",
+            "program":"non-phd"
+        },
+        {
+            "id":13,
+            "name":"SIN JEAN MIN",
+            "short_name":"Jean",
+            "program":"non-phd"
+        },
+        {
+            "id":14,
+            "name":"TAN YI ZHEN",
+            "short_name":"Yizhen",
+            "program":"phd"
+        },
+        {
+            "id":15,
+            "name":"TEO WEI JIAN AUTUMN",
+            "short_name":"Autumn",
+            "program":"non-phd"
+        },
+        {
+            "id":16,
+            "name":"WANNIARACHCHIGE DHANUJA THARITH WANNIARACHCHI",
+            "short_name":"Dhanuja",
+            "program":"phd"
+        },
+        {
+            "id":17,
+            "name":"ZHANG SONGHENG",
+            "short_name":"Alexander",
+            "program":"phd"
+        }
+    ]
+}
